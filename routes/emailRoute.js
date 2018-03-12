c86ffc38d298b120803bcedb0c4a3a4c4b7c4d12
spurthihegdeh <spurthi.hegde@gmail.com>
2018-03-12 10:58:19 +0530

    Removed unwanted piece of code

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -39,36 +39,6 @@ var nodemailer = require('nodemailer');
                 
     });
 
-    
-    
-   
-   
-    
-
-router.get('/custData',function(req,res){
-    res.json('Customer Data getting');
-})
-
-router.post('/sampleApp4', function(req,res)
-{
-  var customerInfo= new customerVar();
-  customerInfo.name= req.body.name;
-  customerInfo.component= req.body.component;
-  customerInfo.place= req.body.place;
-  customerInfo.latitude= req.body.latitude;
-  customerInfo.longitude= req.body.longitude;
-
-
-customerInfo.save()
-.then(item => {
-res.send("item saved to database");
-})
-.catch(err => {
-res.status(400).send("unable to save to database");
-});
-});
-
-
 
 
 module.exports = router;
